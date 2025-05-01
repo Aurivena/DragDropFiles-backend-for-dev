d41f81e2f83daa1db340c7efbdb42a1bdade5441
Konovalov Vlad <vladkonovalov20060@gmail.com>
2025-05-01 14:50:21 +0500

    обновляет makeFile и sql

--- Makefile
+++ Makefile
@@ -1,10 +1,3 @@
-.PHONY: restart clean up
-
-clean:
-	docker-compose down --volumes --remove-orphans
-	docker system prune -af --volumes
-
-up:
-	docker-compose up --build -d
-
-restart: clean up
+restart:
+	docker compose down -v
+	docker compose up
--- sql_scripts/init.sql
+++ sql_scripts/init.sql
@@ -1,12 +1,13 @@
 CREATE TABLE "File"(
    id varchar PRIMARY KEY,
    name varchar NOT NULL ,
-   data_base64 varchar NOT NULL ,
+   mime_type varchar NOT NULL ,
    session varchar NOT NULL ,
    password varchar,
    date_deleted date,
    count_download int
 );
 
+CREATE UNIQUE INDEX unique_session_name ON "File"(session, name);
 
 CREATE USER dragdrop_api WITH PASSWORD '1234';
